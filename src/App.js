8eabbc1b914a8fd86677add2fc369cef56f4bcd3
vijayrachakonda <33103922+vijayrachakonda@users.noreply.github.com>
2020-12-18 00:49:40 -0500

    Makes an Authorizer component that handles Apple Music Authentication

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 // import 'assets/styles/App.scss';
 import './assets/styles/App.scss';
 import React from 'react'
+import Authorizer from './components/Authorizer';
 
 class App extends React.Component {
+
   render() {
     return (
       <div className="App">
-        Music Connect
+        <h1>Music Connect</h1>
+        <Authorizer />
       </div>
     );
   }
+
+
 }
 
 export default App;
---
+++ src/components/Authorizer.js
@@ -0,0 +1,56 @@
+import '../assets/styles/App.scss';
+import React from 'react'
+
+class Authorizer extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+          music: null
+        }
+        this.handleAuthorize = this.handleAuthorize.bind(this);
+    }
+
+    //simply adding a didMount function to load a script tag into the dom in order to access the Music Kit JS lib
+    componentDidMount () {
+        const script = document.createElement("script");
+        script.src = "https://js-cdn.music.apple.com/musickit/v1/musickit.js";
+        script.async = true;
+        document.body.appendChild(script);
+
+        document.addEventListener('musickitloaded', () => {
+            this.setState({music: window.MusicKit.configure({
+                developerToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjI4TURDWTcyVFAifQ.eyJpc3MiOiI0Q1JDNEdGUVpXIiwiZXhwIjoxNjA4Mjg0MzU0LCJpYXQiOjE2MDgyNDExNTR9.nnv23e9I7dr8Yz1dmLvy1koQZhwE0Pe83GZuJ5Ne_LJWx85TAaLibHIhs8PDZujBxt6mQ_POmFqnjd-7e06HeA',
+                app: {
+                  name: 'Playlist Connect',
+                  build: '0.0.1'
+                }
+              })
+            }); 
+        });
+
+      }
+
+
+      //click handler that prompts user for authorization upon clicking it
+    handleAuthorize() {
+        this.state.music.authorize().then(musicUserToken => {
+            console.log(`Authorized, music-user-token: ${musicUserToken}`);
+        });
+    }
+
+    render() {
+        return(
+            <div>
+                <button onClick={this.handleAuthorize}>
+                    Authorize me!
+                </button>
+            </div>
+        )
+    }
+
+
+
+
+}
+
+export default Authorizer;
