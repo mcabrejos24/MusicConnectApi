a1d634a260557c9da06f598e8ac0fa60293331ac
Manuel Cabrejos <manuelcabrejos97@gmail.com>
2020-12-25 16:24:48 -0500

    creates and styles ConnectOptions component

--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -1,19 +1,29 @@
 import React from 'react';
-import Authorizer from './../../components/Authorizer';
-import Authorizer_Spotify from './../../components/Authorizer_Spotify';
-import Navbar from './components/Navbar';
+// import Authorizer from './../../components/Authorizer';
+// import Authorizer_Spotify from './../../components/Authorizer_Spotify';
+import ConnectOptions from './components/ConnectOptions';
+import cool_guitar from './../../assets/img/cool_guitar.jpg';
+import cool_piano from './../../assets/img/cool_piano.jpg';
+import cool_drum from './../../assets/img/cool_drum.jpg';
+
 
 export default function Home() {
 
 
     return (
-        <div className="App">
-            <Navbar />
-            <h1>Music Connect</h1>
-            <br></br>
+        <div className="content">
+
+            {/* <h1>Music Connect</h1>
             <Authorizer />
-            <br></br>
-            <Authorizer_Spotify />
+            <Authorizer_Spotify /> */}
+
+            {/* <h1>CONNECT YOUR MUSIC!</h1> */}
+
+            <div className="options-wrapper" >
+                <ConnectOptions description="Already have a Spotify and Apple Music playlist available" image={cool_guitar} imageAlt="Cool Guitar" url="https://www.google.com"/>
+                <ConnectOptions description="Only have a Spotify or Apple Music playlist available" image={cool_piano} imageAlt="Cool Piano" url="https://www.google.com"/>
+                <ConnectOptions description="Have neither Spotify or Apple Music playlist available" image={cool_drum} imageAlt="Cool Drum" url="https://www.google.com"/>
+            </div>
         </div>
     );
 }
