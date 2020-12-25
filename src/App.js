5d288aa5ce56a6f01835ccc9cae103697106afd1
Manuel Cabrejos <manuelcabrejos97@gmail.com>
2020-12-25 16:24:26 -0500

    sets up styling for the options wrapper and content

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './scenes/Home/';
 import Redirect from './scenes/Redirect';
+import Navbar from './components/Navbar';
 
 class App extends React.Component {
 
   render() {
     return (
       <BrowserRouter>
-        <div className="main">
+        <div className="container">
+          <Navbar />
           <Switch>
             <Route path="/" component={Home} exact={true} />
             <Route path="/redirect" component={Redirect} />
