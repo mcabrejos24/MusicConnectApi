58eb41be29d8f8dac48b8583129f7e2df479b9fa
Manuel Cabrejos <manuelcabrejos97@gmail.com>
2020-12-24 19:47:59 -0500

    adds redirect page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 // import 'assets/styles/App.scss';
 import './assets/styles/App.scss';
 import React from 'react'
-
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './scenes/Home/';
+import Redirect from './scenes/Redirect';
 
 class App extends React.Component {
 
@@ -12,14 +12,13 @@ class App extends React.Component {
       <BrowserRouter>
         <div className="main">
           <Switch>
-            <Route path="/" component={Home} exact={true}/>
+            <Route path="/" component={Home} exact={true} />
+            <Route path="/redirect" component={Redirect} />
           </Switch>
         </div>
       </BrowserRouter>
     );
   }
-
-
 }
 
 export default App;
