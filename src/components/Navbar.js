80ad26340bf618433c74edc8e530b7f83d503a12
Manuel Cabrejos <manuelcabrejos97@gmail.com>
2020-12-25 15:25:00 -0500

    changes navbar component location and updates it

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,9 @@ export default function Navbar() {
     
     return(
         <header id="top" className="w-full flex flex-col bg-gray-800 fixed sm:relative pin-t pin-r pin-l">
-            <nav id="site-menu" className="flex flex-col sm:flex-row w-full justify-between items-center px-4 sm:px-6 py-1 bg-dark shadow sm:shadow-none border-b-4 border-teal-500">
+            <nav id="site-menu" className="flex flex-col sm:flex-row w-full justify-between items-center px-4 sm:px-6 py-1 bg-dark shadow sm:shadow-none border-teal-500">
                 <div className="w-full sm:w-auto self-start sm:self-center flex flex-row sm:flex-none flex-no-wrap justify-between items-center">
-                    <a href="#" className="no-underline py-1"><h1 className="font-bold text-gray-100 text-lg">Playlist Connect</h1></a>
+                    <a href="/#" className="no-underline py-1"><h1 className="font-bold text-gray-100 text-lg">Playlist Connect</h1></a>
                     <button id="menuBtn" className="hamburger block sm:hidden focus:outline-none" type="button" onclick="navToggle();">
                         <span className="hamburger__top-bun"></span><span className="hamburger__bottom-bun"></span>
                     </button>
