7ad3f4cc409db6e36aef235ebf4355a518d66e45
Manuel Cabrejos <manuelcabrejos97@gmail.com>
2020-12-26 00:46:00 -0500

    updates font size of connect options component

--- src/scenes/Home/components/ConnectOptions.js
+++ src/scenes/Home/components/ConnectOptions.js
@@ -10,7 +10,7 @@ export default function ConnectOptions(props) {
         <div className="Connect-Options rounded-lg h-60 md:h-48 lg:h-64 xl:h-80  ">
             <a href={url}>
                 <img src={image} alt={imageAlt} className="connect-options-img rounded-lg w-full h-full"/>
-                {/* <p className="options-description">{description}</p> */}
+                <p className="options-description text-gray-100 text-xl md:text-base lg:text-xl xl:text-2xl 2xl:text-3xl">{description}</p>
             </a>
         </div>
     );
