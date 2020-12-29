153c25f956d4f8510892e10b75fae1043dd9c751
vijayrachakonda <33103922+vijayrachakonda@users.noreply.github.com>
2020-12-29 15:04:47 -0500

    Change name of AppleAuthorizer

--- src/components/AuthorizerAppleMusic.js
+++ src/components/AuthorizerAppleMusic.js
@@ -1,7 +1,7 @@
 import '../assets/styles/App.scss';
 import React from 'react'
 
-class Authorizer_AppleMusic extends React.Component {
+class AuthorizerAppleMusic extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
@@ -16,15 +16,19 @@ class Authorizer_AppleMusic extends React.Component {
         } = process.env;
         // Adding a didMount function to load a script tag into the dom in order to access the Music Kit JS lib
         document.addEventListener('musickitloaded', () => {
-            this.setState({music: window.MusicKit.configure({
-                developerToken: DEVELOPER_TOKEN,
+            window.MusicKit.configure({
+                developerToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjI4TURDWTcyVFAifQ.eyJpc3MiOiI0Q1JDNEdGUVpXIiwiZXhwIjoxNjExMDg0NDE2LCJpYXQiOjE2MDg2NjUyMTZ9.Hxy_XRE1zhgTqRD57u-OSua6T7FoCPOm8ONqKrheoGHw_GUwehiIXEvebpb3II7fXOqGQxDAlL7w61tPZmLLeQ',
                 app: {
                   name: 'Playlist Connect',
                   build: '0.0.1'
                 }
-              })
-            }); 
+            });
         });
+        
+        // console.log(window.MusicKit.getInstance());
+        // this.setState({music: window.MusicKit.getInstance()});
+
+        
     }
 
     // Click handler that prompts user for authorization upon clicking it
@@ -45,4 +49,4 @@ class Authorizer_AppleMusic extends React.Component {
     }
 }
 
-export default Authorizer_AppleMusic;
+export default AuthorizerAppleMusic;
