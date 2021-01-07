23c8d61d9cfbedcc8c438e77674d445533067540
Manuel Cabrejos <manuelcabrejos97@gmail.com>
2021-01-07 00:16:07 -0500

    remakes apple authorizer to a functional component

--- src/components/AuthorizerAppleMusic.js
+++ src/components/AuthorizerAppleMusic.js
@@ -1,48 +1,38 @@
 import '../assets/styles/App.scss';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-class AuthorizerAppleMusic extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-          music: null
+export default function AuthorizerAppleMusic(props) {
+    const [music, setMusic] = useState(null);
+
+    useEffect(() => {
+        const { REACT_APP_DEVELOPER_TOKEN } = process.env;
+
+        try {
+            window.MusicKit.configure({
+                developerToken: REACT_APP_DEVELOPER_TOKEN,
+                app: {
+                name: 'Playlist Connect',
+                build: '0.0.1',
+                }
+            });
+            setMusic(window.MusicKit.getInstance());
+        } catch(e) {
+            console.warn(e);
         }
-        this.handleAuthorize = this.handleAuthorize.bind(this);
-        
+    },[]);
+
+    function handleAuthorize() {
+        music.authorize()
+            .then(musicUserToken => {
+                console.log(`Authorized, music-user-token: ${musicUserToken}`);
+            });
     }
 
-    componentDidMount () {
-        const {
-            REACT_APP_DEVELOPER_TOKEN
-        } = process.env;
-        // Adding a didMount function to load a script tag into the dom in order to access the Music Kit JS lib        
-        window.MusicKit.configure({
-            developerToken: REACT_APP_DEVELOPER_TOKEN,
-            app: {
-              name: 'Playlist Connect',
-              build: '0.0.1'
-            }
-        });
-        
-        this.setState({music: window.MusicKit.getInstance()});
-    }
-
-    // Click handler that prompts user for authorization upon clicking it
-    handleAuthorize() {
-        this.state.music.authorize().then(musicUserToken => {
-            console.log(`Authorized, music-user-token: ${musicUserToken}`);
-        });
-    }
-
-    render() {
-        return(
-            <div className="text-xl">
-                <button onClick={this.handleAuthorize} className="authorizer authorizer-applemusic">
-                    Authorize with Apple Music
-                </button>
-            </div>
-        )
-    }
+    return(
+        <div className="text-xl">
+            <button onClick={handleAuthorize} className="authorizer authorizer-applemusic">
+                Authorize with Apple Music
+            </button>
+        </div>
+    );
 }
-
-export default AuthorizerAppleMusic;
