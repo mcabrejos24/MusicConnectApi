132e850568fad10cca6c27c07888b13ef773c021
Manuel Cabrejos <manuelcabrejos97@gmail.com>
2021-01-08 21:47:58 -0500

    adds spotify api authorizer functionality

--- src/components/AuthorizerSpotify.js
+++ src/components/AuthorizerSpotify.js
@@ -8,32 +8,62 @@ import '../assets/styles/components/authorizer.scss'
 
 
 export default function Authorizer_Spotify(props) {
-    const { setter } = props;
 
-    useEffect(() => {
-        setter(true);
-    });
+    const token = window.location.hash.substr(1).split('&')[0].split("=")[1];
 
+      if (token) {
+        window.opener.spotifyCallback(token)
+    } 
+
+    
     const {
         REACT_APP_CLIENT_ID,
         REACT_APP_AUTHORIZE_URL,
         REACT_APP_REDIRECT_URL
     } = process.env;
 
-//    useEffect(() => {
-//     axios.get(`https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=code&redirect_uri=${redirect_uri}&scope=user-read-private%20user-read-email&state=34fFs29kd09`)
-//         .then(res => {
-//             console.log('spiderman: '+res);
-//         })
-//     },[]);
+    const url = `${REACT_APP_AUTHORIZE_URL}?client_id=${REACT_APP_CLIENT_ID}&response_type=token&redirect_uri=${REACT_APP_REDIRECT_URL}&scope=user-top-read&show_dialog=true`;
 
-    const handleLogin = () => {
-        window.location = `${REACT_APP_AUTHORIZE_URL}?client_id=${REACT_APP_CLIENT_ID}&redirect_uri=${REACT_APP_REDIRECT_URL}&response_type=token&show_dialog=true`;
-    };
+    useEffect(() => {
+        const { setter } = props;
+
+        window.spotifyCallback = (payload) => {
+            window.$popup.close();
+            console.log('token: '+payload);
+            setter(true);
+            // fetch('https://api.spotify.com/v1/me', {
+            //   headers: {
+            //     'Authorization': `Bearer ${payload}`
+            //   }
+            // }).then(response => {
+            //   return response.json()
+            // }).then(data => {
+            //   // do something with data
+            //   console.warn(data);
+            // })
+        }
+    },[props]);
+
+    function login() {
+        window.$popup = window.open(
+            url,
+            'Login with Spotify',
+            'width=800,height=600'
+        )
+    }
+    
+
+
+  
+
+
+    // const handleLogin = () => {
+    //     window.location = `${REACT_APP_AUTHORIZE_URL}?client_id=${REACT_APP_CLIENT_ID}&redirect_uri=${REACT_APP_REDIRECT_URL}&response_type=token&show_dialog=true`;
+    // };
 
     return (
         <div className="text-xl">
-            <button variant="info" type="submit" onClick={handleLogin} className="authorizer authorizer-spotify">
+            <button variant="info" type="submit" onClick={login} className="authorizer authorizer-spotify">
                 Authorize with Spotify
             </button>
         </div>
