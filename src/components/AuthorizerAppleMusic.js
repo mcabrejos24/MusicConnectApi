981600f2f64e80355417f1f0dbc7e6cc165039f1
Manuel Cabrejos <manuelcabrejos97@gmail.com>
2021-01-09 00:27:46 -0500

    fixes the apple music bug that failed at configure

--- src/components/AuthorizerAppleMusic.js
+++ src/components/AuthorizerAppleMusic.js
@@ -1,31 +1,26 @@
 import '../assets/styles/App.scss';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 
 export default function AuthorizerAppleMusic(props) {
-    const [music, setMusic] = useState(null);
+    const { setter } = props;
 
     useEffect(() => {
         const { REACT_APP_DEVELOPER_TOKEN } = process.env;
-
-        try {
             window.MusicKit.configure({
                 developerToken: REACT_APP_DEVELOPER_TOKEN,
                 app: {
-                name: 'Playlist Connect',
-                build: '0.0.1',
+                    name: 'Playlist Connect',
+                    build: '0.0.1',
                 }
             });
-            setMusic(window.MusicKit.getInstance());
-        } catch(e) {
-            console.warn(e);
-        }
-    },[]);
+    }, []);
 
     function handleAuthorize() {
-        music.authorize()
+        window.MusicKit.getInstance().authorize()
             .then(musicUserToken => {
                 console.log(`Authorized, music-user-token: ${musicUserToken}`);
-            });
+                setter(true);
+        });
     }
 
     return(
