376b69eba24bacec0a81b5720f3e29bd999460eb
Manuel Cabrejos <manuelcabrejos97@gmail.com>
2021-01-10 15:59:15 -0500

    adds comment to searchSpotifyAPI function

--- src/api/searchSpotifyAPI.js
+++ src/api/searchSpotifyAPI.js
@@ -1,7 +1,6 @@
-
-
-
 export default function containsPlaylistSpotify(playlistName) {
     // console.log('searching for playlist in spotify');
+    //authValue referenced in here with the playlistName being passed through api path
+    
     return false // will be true if contains false if not
 }
