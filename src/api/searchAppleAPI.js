1317474839c60a2017b77a6c86319da55a7088c0
vijayrachakonda <33103922+vijayrachakonda@users.noreply.github.com>
2021-01-25 21:27:56 -0500

    Deletes comments in search api - apple

--- src/api/searchAppleAPI.js
+++ src/api/searchAppleAPI.js
@@ -22,18 +22,5 @@ export default function containsPlaylistApple(playlistName) {
             }
         })
         return returnValue;
-    })
-
-    // let returnValue = false;
-    // let mk = window.MusicKit.getInstance();
-
-    // return mk.api.library.playlists().then(function(data) {
-    //     data.forEach(playlist => {
-    //       if (playlist.attributes.name.toLowerCase() === playlistName.toLowerCase()) {
-    //           returnValue = true
-    //           return;
-    //       }
-    //     });
-    //     return returnValue;
-    // });
+    });
 }
