fbc11c2d5768bd3fb65359db56c935401c01b3c1
Manuel Cabrejos <manuelcabrejos97@gmail.com>
2021-02-17 01:55:35 -0500

    creates music-card-2 and makes email page into email and text page

--- src/scenes/HomeSPA/TextEmail/index.js
+++ src/scenes/HomeSPA/TextEmail/index.js
@@ -1,11 +1,24 @@
+import './style.scss';
+import { useState } from 'react';
 import BackButton from '../components/BackButton';
+import MusicCard2 from '../../../components/MusicCard2';
+import AuthorizerAppleMusic from '../../../components/AuthorizerAppleMusic';
+import AuthorizerSpotify from '../../../components/AuthorizerSpotify';
 
 export default function TextEmail() {
+
+    const [playlistReady, setPlaylistReady] = useState(false);
+    // console.log(playlistReady); pass this into submit button 
 
     return (
         <div className="content">
             <BackButton></BackButton>
-            <p>Email Page</p>
+            <div className="double-authorizer-wrapper">
+                <MusicCard2 confirmPlaylist={setPlaylistReady}>
+                        <AuthorizerAppleMusic />
+                        <AuthorizerSpotify />
+                </MusicCard2>
+            </div>
         </div>
     );
 }
