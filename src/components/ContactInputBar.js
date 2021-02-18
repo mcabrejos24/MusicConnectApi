3a7bddc8d75819730bca3c6229f120457c155128
Manuel Cabrejos <manuelcabrejos97@gmail.com>
2021-02-17 22:50:42 -0500

    updates contactInput email regex format

--- src/components/ContactInputBar.js
+++ src/components/ContactInputBar.js
@@ -130,15 +130,16 @@ export default function ContactInputBar(props) {
 
   function validateEmail(email) {
     let inputElementWrapper = document.querySelector(`.contact-input-wrapper`);
-    if (!email) {
-      // resets style
+    if (!email) { // resets style
       inputElementWrapper.classList.remove("input-contains");
       inputElementWrapper.classList.remove("input-does-not-contain");
       return;
     }
-    const inputIsEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-      email.toLowerCase()
-    );
+    
+    // Check to see if email is in the correct format ex: email@someEmail.com, school@live.unc.edu, etc.
+    const emailFormat = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+    const inputIsEmail = emailFormat.test(email.toLowerCase());
+
     if (inputIsEmail) {
       inputElementWrapper.classList.add("input-contains");
       if (inputElementWrapper.classList.contains("input-does-not-contain"))
