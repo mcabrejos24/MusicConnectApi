37ca64ad022b8df08c8a4d0714cd533e447004aa
Manuel Cabrejos <manuelcabrejos97@gmail.com>
2021-02-21 21:40:13 -0500

    updates the 3rd connect options which is the SyncID page

--- src/scenes/HomeSPA/SyncID/index.js
+++ src/scenes/HomeSPA/SyncID/index.js
@@ -1,11 +1,25 @@
+import './style.scss';
+import { useState } from 'react';
 import BackButton from '../components/BackButton';
+import MusicCard3 from '../../../components/MusicCard3';
+import AuthorizerAppleMusic from '../../../components/AuthorizerAppleMusic';
+import AuthorizerSpotify from '../../../components/AuthorizerSpotify';
+import SubmitSync from '../../../components/SubmitSync';
 
 export default function SyncID() {
-
+    const [playlistReady, setPlaylistReady] = useState(false);
+    
     return (
         <div className="content">
             <BackButton></BackButton>
-            <p>Texting Page</p>
+            <div className="double-authorizer-wrapper">
+                <MusicCard3 confirmPlaylist={setPlaylistReady} >
+                        <AuthorizerAppleMusic />
+                        <AuthorizerSpotify />
+                </MusicCard3>
+            </div>
+            <SubmitSync playlistReady={ playlistReady }/>
+
         </div>
     );
 }
