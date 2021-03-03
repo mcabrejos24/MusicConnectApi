1704a18187d6d62b436427c3996b490c01a2567c
vijayrachakonda <33103922+vijayrachakonda@users.noreply.github.com>
2021-03-02 20:48:26 -0500

    Sets up endpoint files

--- backend/playlistConnect/api/views.py
+++ backend/playlistConnect/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
 
 # Create your views here.
+
+
