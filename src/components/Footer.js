e545624aa2951fe3d235a54963e2e22074ecba71
Manuel Cabrejos <manuelcabrejos97@gmail.com>
2021-03-31 00:56:06 -0400

    creates and styles the complete footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+export default function Footer() {
+    
+    return(
+        <footer className="footer">
+            <div className="footer--wrapper">
+                <div className="copyright">Copyright © 2021 Manuel Cabrejos, Giselle Cabrejos, Vijay Rachakonda</div>
+                <div className="legal-links">
+                    <a className="privacy-policy">Privacy Policy</a>
+                    <a className="terms-of-use">Terms of Use</a>
+                    <a className="contact-us" href="/contact-us/" >Contact Us</a>
+                    <a className="site-map">Site Map</a>
+                </div>
+            </div>
+        </footer>
+    );
+} 
