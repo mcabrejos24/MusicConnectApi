ba6ec0717c04402c8b899db17bf4d5836b2a8916
vijayrachakonda <33103922+vijayrachakonda@users.noreply.github.com>
2021-04-06 08:55:52 -0400

    Sets up initial post endpoint for syncing

--- backend/playlistConnect/api/views.py
+++ backend/playlistConnect/api/views.py
@@ -4,6 +4,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
+import ast 
+import requests
+import json
 
 # Create your views here.
 
@@ -13,16 +16,36 @@ def health_check(request):
 
 
 class Pairing(APIView):
+    apple_url = "https://api.music.apple.com/v1/me/library/playlists"
+    payload={}
 
+    #the post endpoint 'api/pairing'
     def post(self, request, format=None):
-        spotify_obj = request.data['attributes']['spotifyObject']
-        apple_obj = request.data['attributes']['appleObject']
+        spotify_obj = ast.literal_eval(request.data['attributes']['spotifyObject'])
+        apple_obj = ast.literal_eval(request.data['attributes']['appleObject'])
         self.get_songs(spotify_obj, apple_obj)
         return Response(request.data)
 
+    # Gets the apple and spotify lists of songs for merging (not complete)
     def get_songs(self, spotify_obj,apple_obj):
-        print(spotify_obj)
-        print(apple_obj)
+        headers = {
+            'music-user-token': apple_obj['auth'],
+            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjI4TURDWTcyVFAifQ.eyJpc3MiOiI0Q1JDNEdGUVpXIiwiZXhwIjoxNjIxMDg5NTM2LCJpYXQiOjE2MTYyNTExMzZ9.YuRqACMwkbHUdi1KPdFlzBFWP03DYj027Wvxwb9LvqwpRPVMLZIUEP-y7wenzpmW-vjwtucJgB9VNzk7l5kQDg'
+        }
+        apple_response = requests.request("GET", self.apple_url, headers=headers, data=self.payload)
+        apple_response = apple_response.json()
+        apple_target_playlist = self.apple_search(apple_obj['playlist'], apple_response)
+        # 
+        print(apple_target_playlist)
+
+    # Gets apple playlist from user's library
+    def apple_search(self, playlist_name, apple_response):
+        for i in range(len(apple_response['data'])):
+            print((apple_response['data'][i]['attributes']['name']).lower())
+            if playlist_name == (apple_response['data'][i]['attributes']['name']).lower():
+                return apple_response['data'][i]['id']
+        
+    
 
 
 # One greater function pairs that recieves all the parameters we need (2 objects: Spotify and Apple)
--- backend/playlistConnect/playlistConnect/settings.py
+++ backend/playlistConnect/playlistConnect/settings.py
@@ -54,6 +54,8 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware'
 ]
 
+# CORS_ORIGIN_ALLOW_ALL = True
+
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000'
 ]
