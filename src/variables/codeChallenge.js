cfa6d7a02472156bb1f7779729d51c5d6fc180f7
mcabrejos24 <manuelcabrejos97@gmail.com>
2022-03-03 21:30:48 -0500

    updates codeChallenge file

--- src/variables/codeChallenge.js
+++ src/variables/codeChallenge.js
@@ -1,16 +1,34 @@
-import { sha256 } from './sha256';
-import { setCodeVerifier, getCodeVerifier } from './authValues';
 
-function generateNonce() {
-    const chars = 'abcdefghijklmnopqrstuvwxyz123456789_.-~';
-    let nonce = '';
-    for (let i=0; i<100; i++) {
-        nonce += chars.charAt(Math.random() * (chars.length));
+function randomize(length, mask) {
+    let result = "";
+    let randomIndices = new Int8Array(length);
+    window.crypto.getRandomValues(randomIndices);
+    const byteLength = 256;
+    const maskLength = Math.min(mask.length, byteLength);
+    const scalingFactor = byteLength / maskLength;
+
+    for (var i = 0; i < length; i++) {
+        result += mask[Math.floor(Math.abs(randomIndices[i]) / scalingFactor)];
     }
-    setCodeVerifier(nonce);
+    return result;
 }
 
-export function generateCodeChallenge() {
-    generateNonce();
-    return sha256(getCodeVerifier());
+function generateVerifier(length) {
+    const mask = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._~";
+    return randomize(length, mask);
+}
+
+function base64UrlEncode(array) {
+    return window.btoa(String.fromCharCode.apply(null, new Uint8Array(array)))
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=+$/, '');
+}
+
+export function generateChallenge(length = 43) {
+    let verifier = generateVerifier(length);
+
+    const encoder = new TextEncoder();
+    const data = encoder.encode(verifier);
+    return window.crypto.subtle.digest('SHA-256', data).then(array => { return { code_challenge: base64UrlEncode(array), code_verifier: verifier }; });
 }
