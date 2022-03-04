803473d3765e4a2b1bc15412b24bd85224ad6b21
mcabrejos24 <manuelcabrejos97@gmail.com>
2022-03-03 21:29:50 -0500

    updates authorizer spotify to take code and provide auth token

--- src/components/AuthorizerSpotify.js
+++ src/components/AuthorizerSpotify.js
@@ -1,22 +1,25 @@
+// import axios from 'axios';
 import { useEffect } from 'react';
-import { setAuthValue, getAuthValue } from '../variables/authValues';
-import { generateCodeChallenge } from '../variables/codeChallenge';
+import { setAuthValue, getSpotifyRefreshToken, setSpotifyRefreshToken } from '../variables/authValues';
+import { generateChallenge } from '../variables/codeChallenge';
+var axios = require('axios');
+var qs = require('qs');
+var verify;
 
 export default function AuthorizerSpotify(props) {
     const {
         REACT_APP_CLIENT_ID,
+        REACT_APP_CLIENT_SECRET,
         REACT_APP_AUTHORIZE_URL,
         REACT_APP_REDIRECT_URL,
         REACT_APP_URL_STATE
     } = process.env;
-    const codeChallange = generateCodeChallenge();
     const scopes = 'playlist-modify-private%20playlist-read-collaborative%20playlist-read-private%20playlist-modify-public'
-    const url = `${REACT_APP_AUTHORIZE_URL}?client_id=${REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${REACT_APP_REDIRECT_URL}&state=${REACT_APP_URL_STATE}&scope=${scopes}&show_dialog=true&code_challenge_method=S256&code_challenge=${codeChallange}`;
 
     useEffect(() => {
         const { setter } = props;
-
-        window.spotifyCallback = (payload) => { // called if spotify successfully redirects
+        
+        window.spotifyCallback = async (payload) => { // called if spotify successfully redirects
             window.$popup.close();
             if (payload === 'failedParams') {
                 console.error('Failed to parse Spotify API response and get values');
@@ -28,20 +31,56 @@ export default function AuthorizerSpotify(props) {
                 console.error('User denied access');
                 return;
             }
-            setter("spotify"); // maybe instead of setter that passes setShowInput to MusicCard, we can change the styles right here, making the input hidden or not
-            let successSettingPayload = setAuthValue('spotify', payload);
-            if(!successSettingPayload) console.error('Failed to set Authenticated Code Value');
+            let auth_token = await requestAccesToken(payload);
+            if (auth_token && auth_token.access_token) {
+                setter("spotify"); // maybe instead of setter that passes setShowInput to MusicCard, we can change the styles right here, making the input hidden or not
+                let successSettingPayload = setAuthValue('spotify', auth_token.access_token);
+                if(!successSettingPayload) console.error('Failed to set Authenticated Code Value');
+            }
         }
-
     },[]);
 
-    function login() {
-        // opens window in a different window (diff tab if in full screen)
-        window.$popup = window.open(
+    async function login() {
+        let codeChallenge = await generateChallenge();
+        verify = codeChallenge.code_verifier;
+        
+        const url = `${REACT_APP_AUTHORIZE_URL}?client_id=${REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${REACT_APP_REDIRECT_URL}&state=${REACT_APP_URL_STATE}&scope=${scopes}&show_dialog=true&code_challenge_method=S256&code_challenge=${codeChallenge.code_challenge}`;
+
+        window.$popup = window.open( // opens window in a different window (diff tab if in full screen)
             url,
             'Login with Spotify',
             'width=400,height=650,left=-250'
         )
+    }
+    
+    function requestAccesToken(code) {
+        let authorization = window.btoa(`${REACT_APP_CLIENT_ID}:${REACT_APP_CLIENT_SECRET}`);
+        let data = qs.stringify({
+            'code': code,
+            'redirect_uri': REACT_APP_REDIRECT_URL,
+            'grant_type': 'authorization_code',
+            'client_id': REACT_APP_CLIENT_ID,
+            'code_verifier': verify 
+        });
+        return axios(
+            {
+                method: 'post',
+                url: 'https://accounts.spotify.com/api/token',
+                headers: { 
+                'Authorization': `Basic ${authorization}`, 
+                'Content-Type': 'application/x-www-form-urlencoded', 
+                },
+                data : data
+            }
+        )
+        .then(function (response) {
+            return response.data;
+
+        })
+        .catch(function (error) {
+            return error;
+        });
+        
     }
     
     return (
