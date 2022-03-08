2f488131781062c322264338e322dd0a82195a3e
mcabrejos24 <manuelcabrejos97@gmail.com>
2022-03-07 23:53:41 -0500

    updates redirect href slice for live

--- src/scenes/Redirect/index.js
+++ src/scenes/Redirect/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 export default function Redirect() {
     const { REACT_APP_URL_STATE } = process.env;
-    const params = window.location.href.slice(31,window.location.href.length).split('&'); // extracts code and state from query params in the redirect from the spotify api
+    if (window.location.href.indexOf('redirect') == -1) {
+        window.opener.spotifyCallback('failedParams');
+        return <div></div>
+    }
+
+    const params = window.location.href.slice(window.location.href.indexOf('redirect')+9,window.location.href.length).split('&'); // extracts code and state from query params in the redirect from the spotify api
 
     if (!params || !params[0] || !params[1] || !params[0].split('=') || !params[1].split('=') || !params[0].split('=')[1] || !params[1].split('=')[1] ) {
         window.opener.spotifyCallback('failedParams'); //failed to get param values, send message to parent window
