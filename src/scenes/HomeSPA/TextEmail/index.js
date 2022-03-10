0c2d88deacd32c9e1e7165fceab20ce23fe22c73
mcabrejos24 <manuelcabrejos97@gmail.com>
2022-03-10 18:58:54 -0500

    adds coming soon pop up

--- src/scenes/HomeSPA/TextEmail/index.js
+++ src/scenes/HomeSPA/TextEmail/index.js
@@ -9,8 +9,34 @@ export default function TextEmail() {
   const [playlistReady, setPlaylistReady] = useState(false);
   const [contactReady, setContactReady] = useState(false);
 
+
+  function openPopUp(pop) {
+    const popup = document.getElementById("popup");
+    popup.style.visibility = "visible";
+    popup.style.opacity = "1";
+  }
+
+  function closePopUp() {
+    const popup = document.getElementById("popup");
+    popup.style.visibility = "hidden";
+    popup.style.opacity = "0";
+  }
+
+  setTimeout(openPopUp,500);
+
   return (
-    <div className="content">
+    <div className="content page-text-email">
+
+      <div className="coming-soon">
+        <div id="popup" className="popup">
+          <div className="popup-card">
+            <h2>Coming Soon!</h2>
+            <button id="close" class="close" onClick={closePopUp}>&times;</button>
+            <p className="popup-card--content">This page is still in development so submitting won't actually do anything. But feel free to mess around!</p>
+          </div>
+        </div>
+      </div>
+
       <BackButton></BackButton>
       <div className="double-authorizer-wrapper">
         <MusicCard2
