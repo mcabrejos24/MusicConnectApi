481a3590e5be5e3dcde67f377c34cbfe42a8ae31
mcabrejos24 <manuelcabrejos97@gmail.com>
2022-03-15 19:15:35 -0400

    updates copyright to 2022

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
     return(
         <footer className="footer">
             <div className="footer--wrapper">
-                <div className="copyright">Copyright © 2021 Manuel Cabrejos, Giselle Cabrejos, Vijay Rachakonda</div>
+                <div className="copyright">Copyright © 2022 Manuel Cabrejos, Giselle Cabrejos, Vijay Rachakonda</div>
                 <div className="legal-links">
                     <a className="privacy-policy" href="/privacy-policy">Privacy Policy</a>
                     <a className="terms-of-use" href="/terms-of-use">Terms of Use</a>
