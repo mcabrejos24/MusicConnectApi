9026d7ad537d372cf04ed008ee3d4a9c0345a902
mcabrejos24 <manuelcabrejos97@gmail.com>
2022-03-17 00:03:39 -0400

    updates Redirect page tests

--- src/__tests__/scenes/Redirect.test.js
+++ src/__tests__/scenes/Redirect.test.js
@@ -19,46 +19,44 @@ describe("renders Redirect page", () => {
     });
 });
 
-describe("tests for failed condition of no redirect", () => {
+describe("tests with href, spotifiyCallback and page params", () => {
     let testHref;
     let testSpotifyCallback;
     let page;
     beforeEach(() => {
-        testHref = 'supposed to fail url';
         testSpotifyCallback = (payload) => {
             return payload;
         };    
-        page = shallow(<Redirect testHref={testHref} testSpotifyCallback={testSpotifyCallback}/>);
     });
-    it("redirect not passed into url", () => {
-        expect(page.props().testData).toEqual("no-redirect-in-url");
+    describe("tests for failed condition of no redirect", () => {
+        beforeEach(() => {
+            testHref = 'supposed to fail url';
+            page = shallow(<Redirect testHref={testHref} testSpotifyCallback={testSpotifyCallback}/>);
+        });
+        it("redirect not passed into url", () => {
+            expect(page.props().testData).toEqual("no-redirect-in-url");
+        });
+        it("matches snapshot", () => {
+            expect(toJson(page)).toMatchSnapshot();
+        });
     });
-    it("matches snapshot", () => {
-        expect(toJson(page)).toMatchSnapshot();
+    
+    describe("tests for failed condition of no state and code params", () => {
+        beforeEach(() => {
+            testHref = 'http://localhost:3000/redirect';
+            page = shallow(<Redirect testHref={testHref} testSpotifyCallback={testSpotifyCallback}/>);
+        });
+    
+        it("code and/or state not passed into url", () => {
+            expect(page.props().testData).toEqual("no-code-and-or-state-in-url");
+        });
+        it("matches snapshot", () => {
+            expect(toJson(page)).toMatchSnapshot();
+        });
     });
 });
 
-describe("tests for failed condition of no state and code params", () => {
-    let testHref;
-    let testSpotifyCallback;
-    let page;
-    beforeEach(() => {
-        testHref = 'http://localhost:3000/redirect';
-        testSpotifyCallback = (payload) => {
-            return payload;
-        };    
-        page = shallow(<Redirect testHref={testHref} testSpotifyCallback={testSpotifyCallback}/>);
-    });
-
-    it("code and/or state not passed into url", () => {
-        expect(page.props().testData).toEqual("no-code-and-or-state-in-url");
-    });
-    it("matches snapshot", () => {
-        expect(toJson(page)).toMatchSnapshot();
-    });
-});
-
-describe("tests for failed condition of state not matching", () => {
+describe("tests with testState param added", () => {
     let testHref;
     let testSpotifyCallback;
     let testState;
@@ -68,41 +66,37 @@ describe("tests for failed condition of state not matching", () => {
         testSpotifyCallback = (payload) => {
             return payload;
         };
-        testState = 'stateMeantToFail';
-        page = shallow(<Redirect testHref={testHref} testSpotifyCallback={testSpotifyCallback} testState={testState}/>);
     });
-
-    it("code and/or state not passed into url", () => {
-        expect(page.props().testData).toEqual("state-failed-to-match");
+    describe("tests for failed condition of state not matching", () => {
+        beforeEach(() => {
+            testState = 'stateMeantToFail';
+            page = shallow(<Redirect testHref={testHref} testSpotifyCallback={testSpotifyCallback} testState={testState}/>);
+        });
+    
+        it("code and/or state not passed into url", () => {
+            expect(page.props().testData).toEqual("state-failed-to-match");
+        });
+        it("matches snapshot", () => {
+            expect(toJson(page)).toMatchSnapshot();
+        });
     });
-    it("matches snapshot", () => {
-        expect(toJson(page)).toMatchSnapshot();
-    });
-});
-
-describe("tests for correct render of Redirect page", () => {
-    let testHref;
-    let testSpotifyCallback;
-    let testState;
-    let page;
-    beforeEach(() => {
-        testHref = 'http://localhost:3000/redirect?code=somecode&state=someState';
-        testSpotifyCallback = (payload) => {
-            return payload;
-        };
-        testState = 'someState';
-        page = shallow(<Redirect testHref={testHref} testSpotifyCallback={testSpotifyCallback} testState={testState}/>);
-    });
-    it("page got code sucessfully", () => {
-        expect(page.props().testData).toEqual("render-successful");
-    });
-    it("has correct class", () => {
-        expect(page.props().className).toEqual("content redirect-page");
-    });
-    it("page got code sucessfully", () => {
-        expect(page.find("p").text()).toEqual("Redirect Page");
-    });
-    it("matches snapshot", () => {
-        expect(toJson(page)).toMatchSnapshot();
+    
+    describe("tests for correct render of Redirect page", () => {
+        beforeEach(() => {
+            testState = 'someState';
+            page = shallow(<Redirect testHref={testHref} testSpotifyCallback={testSpotifyCallback} testState={testState}/>);
+        });
+        it("page got code sucessfully", () => {
+            expect(page.props().testData).toEqual("render-successful");
+        });
+        it("has correct class", () => {
+            expect(page.props().className).toEqual("content redirect-page");
+        });
+        it("page got code sucessfully", () => {
+            expect(page.find("p").text()).toEqual("Redirect Page");
+        });
+        it("matches snapshot", () => {
+            expect(toJson(page)).toMatchSnapshot();
+        });
     });
 });
