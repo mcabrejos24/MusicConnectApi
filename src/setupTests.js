443f4f7350d132cef161e1b38542ba08c570de58
mcabrejos24 <manuelcabrejos97@gmail.com>
2022-03-19 23:08:16 -0400

    updates setupTests.js with info on shallow hooks enabling

--- src/setupTests.js
+++ src/setupTests.js
@@ -5,7 +5,23 @@
 import '@testing-library/jest-dom';
 import Enzyme from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-// import enableHooks from 'jest-react-hooks-shallow';
 
 Enzyme.configure({ adapter: new Adapter() });
+
+
+// To render shallow component with hooks, uncomment the first two code below
+// Then import the 'withHooks' function to the test file
+// And wrap it around your shallow function as shown at the bottom
+// Note that hooks will run before the initial render
+// and then regularly on updates
+// ---------------------------------------------------------------------------
+// import enableHooks from 'jest-react-hooks-shallow';
 // enableHooks(jest, { dontMockByDefault: true });
+
+// import { withHooks } from 'jest-react-hooks-shallow';
+
+// beforeEach(() => {
+//      withHooks(() => {
+//         const wrapper = shallow(<Component />);
+//      });
+// });
